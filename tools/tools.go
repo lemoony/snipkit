2d4518686efbd9dca2aa7d937d555ee97af2f20d
Philipp Sessler <sessler.phil@googlemail.com>
2022-06-05 20:32:47 +0200

    Fix github actions

--- tools/tools.go
+++ tools/tools.go
@@ -11,5 +11,6 @@ import (
 	_ "github.com/daixiang0/gci"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/vektra/mockery/v2"
 	_ "mvdan.cc/gofumpt"
 )
